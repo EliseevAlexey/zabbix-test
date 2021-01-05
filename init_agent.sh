7d4fca4d795dfaa2f8cb7f819e9628c69911cb9e
Alexey Eliseev <alexey_eliseev@epam.com>
2021-01-06 02:01:39 +0300

    fixes

--- init_agent.sh
+++ init_agent.sh
@@ -2,6 +2,7 @@
 set -eu
 
 ZBX_SERVER_IP=$1
+HOST_NAME=$(cat /etc/hostname)
 
 echo '--> Install the repository configuration package'
 wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
@@ -17,10 +18,10 @@ echo "--> Configuring agent. Server IP: ${ZBX_SERVER_IP}"
 sudo sed -i "s/Server=127.0.0.1/Server=${ZBX_SERVER_IP}/" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s/ServerActive=127.0.0.1/ServerActive=${ZBX_SERVER_IP}/" /etc/zabbix/zabbix_agentd.conf
 
-echo '--> Configuring UFW to allow connections'
-sudo ufw allow 10050/tcp
-sudo ufw --force enable
-
 echo '--> Restarting zabbix-agent'
 sudo systemctl restart zabbix-agent
 sudo systemctl enable zabbix-agent
+
+echo '--> Configuring UFW to allow connections'
+sudo ufw allow 10050/tcp
+sudo ufw --force enable
