88746037db3f22b62f25fd58eb15be9706e5d652
Alexey Eliseev <alexey_eliseev@epam.com>
2021-01-08 23:15:43 +0300

    fixes

--- install-zabbix-agent.sh
+++ install-zabbix-agent.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eu
 
-ZABBIX_SERVER_HOST=$1
-docker run --rm --name zabbix-agent -e ZBX_HOSTNAME="test-container" -e ZBX_SERVER_HOST="${ZABBIX_SERVER_HOST}" -d -p 10050:10050 zabbix/zabbix-agent
+ZABBIX_SERVER_IP=$1
+HOST_NAME=$(cat /etc/hostname)
 
+echo "Running Zabbix-agent on host '${HOST_NAME}' with connecting to Zabbix server by IP ${ZABBIX_SERVER_IP}"
+docker run \
+    --name zabbix-agent \
+    -e ZBX_HOSTNAME="${HOST_NAME}" -e ZBX_SERVER_HOST="${ZABBIX_SERVER_IP}" \
+    -p 10050:10050 \
+    --rm \
+    -d \
+    zabbix/zabbix-agent
