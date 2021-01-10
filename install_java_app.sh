43957870a1b058065c5ef9f83033131bbd2dd8e5
Alexey Eliseev <alexey_eliseev@epam.com>
2021-01-10 03:08:40 +0300

    fixes

--- install_java_app.sh
+++ install_java_app.sh
@@ -5,5 +5,5 @@ ZABBIX_SERVER_IP=$1
 JMX_PORT=5000
 APP_NAME=v2
 
-docker build --build-arg "${ZABBIX_SERVER_IP}" -t $APP_NAME simple-app/
-docker run --rm -p $JMX_PORT:$JMX_PORT -p 8080 $APP_NAME
+docker build --build-arg ZABBIX_SERVER_IP=${ZABBIX_SERVER_IP} -t $APP_NAME simple-app/
+docker run --rm -p $JMX_PORT:$JMX_PORT -p 8080:8080 $APP_NAME
